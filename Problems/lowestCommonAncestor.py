72ba5eafa1891fc9b10251d6d55769be40a84aac
Nitin Srinivasan <snitin1996@gmail.com>
2020-06-12 15:58:45 -0400

    added lowest common ancestor in bst betw 2 nodes

---
+++ Problems/lowestCommonAncestor.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 15:57:51 2020
+
+@author: nitin
+"""
+
+def lowestCommonAncestor(self, root, p, q):
+    """
+    :type root: TreeNode
+    :type p: TreeNode
+    :type q: TreeNode
+    :rtype: TreeNode
+    """
+
+    while root:
+        if p.val < root.val > q.val:
+            root = root.left
+        elif p.val > root.val < q.val:
+            root = root.right
+        else:
+            return root
+        
+        
+        
